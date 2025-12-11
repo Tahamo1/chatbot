939ff326415d9c0b41ecb2423e67d5bcfab7e229
Venom <tahamo674@gmail.com>
2025-12-11 11:27:07 +0100

    fresh commit

---
+++ src/test/java/com/mohammaditaha/chatbot/ChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohammaditaha.chatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
