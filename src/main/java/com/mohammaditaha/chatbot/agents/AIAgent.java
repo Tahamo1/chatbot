d4f0e44445d89a1baf1fe2c52be29f171c28beac
Venom <tahamo674@gmail.com>
2026-01-11 01:45:29 +0100

    Chatbot Complet

--- src/main/java/com/mohammaditaha/chatbot/agents/AIAgent.java
+++ src/main/java/com/mohammaditaha/chatbot/agents/AIAgent.java
@@ -7,30 +7,35 @@ import org.springframework.ai.tool.ToolCallbackProvider;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 
+import java.util.Arrays;
+
+
 @Component
 public class AIAgent {
     private ChatClient chatClient;
 
-    public AIAgent(ChatClient.Builder builder ,
+    public AIAgent(ChatClient.Builder builder,
                    ChatMemory memory, ToolCallbackProvider tools) {
-        Arrays.Streams(tools.getToolCallbacks()).forEach(toolCallback -> {
-            System.out.println("---------------");
+        Arrays.stream(tools.getToolCallbacks()).forEach(toolCallback -> {
+            System.out.println("----------------------");
             System.out.println(toolCallback.getToolDefinition());
-                    });
+            System.out.println("----------------------");
+        });
         this.chatClient = builder
                 .defaultSystem("""
-                      
-                           Vous un assistant qui se charge de repondre aux questions de l'utilisateur en fonction du contexte fourni .
-                           Si aucun contexte n'est fourni , repond avec JE NE SAIS PAS 
+                        Vous un assistant qui se charge de répondre aux question
+                        de l'utilisateur en fonction du contexte fourni.
+                        Si aucun contexte n'est fourni, répond avec JE NE SAIS PAS         
                         """)
-                .defaultAdvisors(MessageChatMemoryAdvisor.builder(memory).build())
+                .defaultAdvisors(
+                        MessageChatMemoryAdvisor.builder(memory).build())
                 .defaultToolCallbacks(tools)
+
                 .build();
     }
-
-    public Flux<String> askAgent(String query) {
+    public String askAgent(String query) {
         return chatClient.prompt()
                 .user(query)
-                .stream().content();
+                .call().content();
     }
 }
